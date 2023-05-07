9f645bbe9e6db40a2ffaa4b8f3bb292cde26440a
Sandip6509 <sandippatel301@gmail.com>
2023-05-07 14:29:40 +0530

    DataTypes

---
+++ 1_basics/1_variable.js
@@ -0,0 +1,13 @@
+const accountId = 145455;
+let accountEmail = "Sandeeppatel@gmail.com";
+var accountPassword = "654321";
+accountCity = "Ahmedabad";
+let accountState;
+// accountId = 22 // const not allowed;
+
+console.log(accountId);
+
+accountEmail = "Test@mail.com";
+accountPassword = "123pass";
+accountCity = "New Delhi";
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
---
+++ 1_basics/2_datatype.js
@@ -0,0 +1,21 @@
+"use strict" // all js code as new version
+
+console.log("Sandeep Patel");
+
+let name = "Sandeep";
+let age = 32;
+let isLoggedIn = false;
+let state;
+
+// number => 1
+// bigint
+// string
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //null
