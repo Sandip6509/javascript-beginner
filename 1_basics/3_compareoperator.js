5713276e24e012de0693e39425aa673910868ab5
Sandip6509 <sandippatel301@gmail.com>
2023-05-08 21:25:42 +0530

    DataTypes

---
+++ 1_basics/3_compareoperator.js
@@ -0,0 +1,57 @@
+let score = "Sam";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+// "45" => 45
+// "45abc" => NaN
+// true => 1; false => 0;
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **************************** Operations ****************************
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello";
+let str2 = " Sandeep";
+
+let str3 = str1 + str2;
+// console.log(str3);
+// console.log("1" + 3);
+// console.log(1 + "3");
+// console.log("2" + "45");
+// console.log("4" + 2 + 2);
+// console.log(2 + 2 + "5");
+// console.log((2 + 5) *5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1,num2,num3;
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++
+console.log(gameCounter);
